c37ceca6fb562ccf23b84dcc54ca65509391ca5f
Brad Rydzewski <brad.rydzewski@gmail.com>
2020-07-04 10:33:19 -0400

    added placeholder for userdata

--- internal/userdata/userdata.go
+++ internal/userdata/userdata.go
@@ -5,3 +5,28 @@
 // Package userdata contains code to generate userdata scripts
 // executed by the cloud-init directive.
 package userdata
+
+import "fmt"
+
+// Params defines parameters used to create userdata files.
+type Params struct {
+	PublicKey string
+}
+
+// Linux creates a userdata file for the Linux operating system.
+func Linux(params Params) string {
+	return fmt.Sprintf(`#cloud-config
+system_info:
+  default_user: ~
+users:
+- name: root
+  sudo: ALL=(ALL) NOPASSWD:ALL
+  ssh-authorized-keys:
+  - ssh-rsa %s drone@localhost	
+`, params.PublicKey)
+}
+
+// Windows creates a userdata file for the Windows operating system.
+func Windows() string {
+	return ""
+}
