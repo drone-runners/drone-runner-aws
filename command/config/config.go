e7b63659c2a1fe0816b1c5cbf2911a5003ead5d2
Eoin McAfee <eoin.mcafee@harness.io>
2022-05-30 17:00:54 +0100

    update to latest lite-engine

--- command/config/config.go
+++ command/config/config.go
@@ -185,7 +185,7 @@ type EnvConfig struct {
 	}
 
 	Settings struct {
-		LiteEnginePath string `envconfig:"DRONE_LITE_ENGINE_PATH" default:"https://github.com/harness/lite-engine/releases/download/v0.1.0/"`
+		LiteEnginePath string `envconfig:"DRONE_LITE_ENGINE_PATH" default:"https://github.com/harness/lite-engine/releases/download/v0.2.0/"`
 		ReusePool      bool   `envconfig:"DRONE_REUSE_POOL" default:"false"`
 		BusyMaxAge     int64  `envconfig:"DRONE_SETTINGS_BUSY_MAX_AGE" default:"24"`
 		FreeMaxAge     int64  `envconfig:"DRONE_SETTINGS_FREE_MAX_AGE" default:"720"`
