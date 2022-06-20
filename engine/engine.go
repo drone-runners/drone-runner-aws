c03b9dc4d7c9e8b3b133ccd6650505b51122b863
Eoin McAfee <eoin.mcafee@harness.io>
2022-06-20 14:16:04 +0100

    fix bad merge

--- engine/engine.go
+++ engine/engine.go
@@ -130,15 +130,15 @@ func (eng *Engine) Setup(ctx context.Context, specv runtime.Spec) error {
 		Files:     spec.Files,
 	}
 
+	// Currently the OSX m1 architecture does not enable nested virtualisation, so we disable docker.
+	if instance.Platform.OS == oshelp.OSMac && instance.Arch == oshelp.ArchARM64 {
+		*setupRequest.MountDockerSocket = false
+	}
+
 	setupResponse, err := client.Setup(ctx, setupRequest)
 	if err != nil {
 		logr.WithError(err).Errorln("failed to call LE.Setup")
 		return err
-	}
-
-	// Currently the OSX m1 architecture does not enable nested virtualisation, so we disable docker.
-	if instance.Platform.OS == oshelp.OSMac && instance.Arch == oshelp.ArchARM64 {
-		*setupRequest.MountDockerSocket = false
 	}
 
 	logr.WithField("response", fmt.Sprintf("%+v", setupResponse)).
@@ -284,10 +284,7 @@ func (eng *Engine) Run(ctx context.Context, specv runtime.Spec, stepv runtime.St
 	// Currently the OSX m1 architecture does not enable nested virtualisation, so we disable docker.
 	if instance.Platform.OS == oshelp.OSMac && instance.Arch == oshelp.ArchARM64 {
 		*req.MountDockerSocket = false
-	} else {
-		*req.MountDockerSocket = true
 	}
-
 	startStepResponse, err := client.StartStep(ctx, req)
 	if err != nil {
 		logr.WithError(err).Errorln("failed to start step")
