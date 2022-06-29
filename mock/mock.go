40363dbaa06ff8221b680a5249bad7afbe6e6c01
Eoin McAfee <eoin.mcafee@harness.io>
2022-06-29 08:50:27 +0100

    delete mocks

--- mock/mock.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
-package mock
-
-//go:generate mockgen -package=mock -destination=mock_gen.go github.com/drone-runners/drone-runner-aws/store InstanceStore
