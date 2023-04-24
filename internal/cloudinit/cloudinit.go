4b4786b2d35917f10a187a72235273e92fafeac3
Amar Hen <hen.amar@harness.io>
2023-04-24 23:11:09 +0300

    downloading different tmate

--- internal/cloudinit/cloudinit.go
+++ internal/cloudinit/cloudinit.go
@@ -267,11 +267,11 @@ runcmd:
 {{ if .Tmate.Enabled }}
 - 'mkdir /addon'
 {{ if eq .Platform.Arch "amd64" }}
-- 'wget https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-amd64.tar.xz -O /addon/tmate.xz' 
+- 'wget https://github.com/harness/tmate/releases/download/1.0/tmate-1.0-static-linux-amd64.tar.xz  -O /addon/tmate.xz' 
 - 'tar -xf /addon/tmate.xz -C /addon/'
-- 'chmod 777  /addon/tmate-2.4.0-static-linux-amd64/tmate'
-- 'mv  /addon/tmate-2.4.0-static-linux-amd64/tmate /addon/tmate'
-- 'rm -rf /addon/tmate-2.4.0-static-linux-amd64/'
+- 'chmod 777  /addon/tmate-1.0-static-linux-amd64/tmate'
+- 'mv  /addon/tmate-1.0-static-linux-amd64/tmate /addon/tmate'
+- 'rm -rf /addon/tmate-1.0-static-linux-amd64/'
 {{ else if eq .Platform.Arch "arm64" }}
 - 'wget https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-arm64v8.tar.xz -O /addon/tmate.xz' 
 - 'tar -xf /addon/tmate.xz -C /addon/'
@@ -317,11 +317,11 @@ runcmd:
 {{ if .Tmate.Enabled }}
 - 'mkdir /addon'
 {{ if eq .Platform.Arch "amd64" }}
-- 'wget https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-amd64.tar.xz -O /addon/tmate.xz' 
+- 'wget https://github.com/harness/tmate/releases/download/1.0/tmate-1.0-static-linux-amd64.tar.xz  -O /addon/tmate.xz' 
 - 'tar -xf /addon/tmate.xz -C /addon/'
-- 'chmod 777  /addon/tmate-2.4.0-static-linux-amd64/tmate'
-- 'mv  /addon/tmate-2.4.0-static-linux-amd64/tmate /addon/tmate'
-- 'rm -rf /addon/tmate-2.4.0-static-linux-amd64/'
+- 'chmod 777  /addon/tmate-1.0-static-linux-amd64/tmate'
+- 'mv  /addon/tmate-1.0-static-linux-amd64/tmate /addon/tmate'
+- 'rm -rf /addon/tmate-1.0-static-linux-amd64/'
 {{ else if eq .Platform.Arch "arm64" }}
 - 'wget https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-arm64v8.tar.xz -O /addon/tmate.xz' 
 - 'tar -xf /addon/tmate.xz -C /addon/'
