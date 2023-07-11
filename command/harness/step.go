b1eaaa87f6993759282c9f0fbda1324071453ba5
Harsh Jain <harsh.jain@harness.io>
2023-07-11 22:30:22 +0530

    fix lite engine timeout

--- command/harness/step.go
+++ command/harness/step.go
@@ -33,7 +33,7 @@ type ExecuteVMRequest struct {
 }
 
 var (
-	stepTimeout = 4 * time.Hour
+	stepTimeout = 10 * time.Hour
 )
 
 func HandleStep(ctx context.Context, r *ExecuteVMRequest, s store.StageOwnerStore, env *config.EnvConfig, poolManager *drivers.Manager, metrics *metric.Metrics) (*api.PollStepResponse, error) {
