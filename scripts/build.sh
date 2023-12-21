0bf79c364a64558b7e7bf31dcd9af45fe1d06173
abhay084 <abhay.ganvir@harness.io>
2023-12-21 23:26:15 +0530

    feat: [CI-10592]: add helm chart support for dlite

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,8 +10,7 @@ set -e
 set -x
 
 # linux
-#go build -ldflags "-extldflags \"-static\"" -o release/linux/amd64/drone-runner-aws-linux-amd64
+go build -ldflags "-extldflags \"-static\"" -o release/linux/amd64/drone-runner-aws-linux-amd64
 # darwin
-GOARCH=amd64 go build -o ./release/linux/amd64/drone-runner-aws-linux-amd64
-ls -la
+#GOARCH=amd64 go build -o release/linux/amd64/drone-runner-aws-linux-amd64
 
