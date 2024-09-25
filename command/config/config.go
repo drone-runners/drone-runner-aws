b90972ae9f27fae4bb992b70640f6f2cdeab000e
Anurag Madnawat <anurag.madnawat@harness.io>
2024-09-25 13:11:17 -0700

    [feat]: [CI-12024]: Update lite-engine to fix Savings UX bugs (#507)

--- command/config/config.go
+++ command/config/config.go
@@ -349,7 +349,7 @@ type EnvConfig struct {
 		AutoInjectionBinaryURI string `envconfig:"DRONE_HARNESS_AUTO_INJECTION_BINARY_URI"`
 	}
 	LiteEngine struct {
-		Path                string `envconfig:"DRONE_LITE_ENGINE_PATH" default:"https://github.com/harness/lite-engine/releases/download/v0.5.82/"`
+		Path                string `envconfig:"DRONE_LITE_ENGINE_PATH" default:"https://github.com/harness/lite-engine/releases/download/v0.5.83/"`
 		EnableMock          bool   `envconfig:"DRONE_LITE_ENGINE_ENABLE_MOCK"`
 		MockStepTimeoutSecs int    `envconfig:"DRONE_LITE_ENGINE_MOCK_STEP_TIMEOUT_SECS" default:"120"`
 	}
