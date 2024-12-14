e82ddf4cc8c455ba8bfb71c7b1abcbe2ad1f8c2b
abhay084 <abhay.ganvir@harness.io>
2024-12-14 16:24:10 +0530

    fix: [CI-15476]: fix lint

--- engine/linter/linter.go
+++ engine/linter/linter.go
@@ -10,9 +10,9 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/drone-runners/drone-runner-aws/engine/resource"
 	"github.com/drone-runners/drone-runner-aws/app/drivers"
 	"github.com/drone-runners/drone-runner-aws/app/oshelp"
+	"github.com/drone-runners/drone-runner-aws/engine/resource"
 	"github.com/drone/drone-go/drone"
 	"github.com/drone/runner-go/manifest"
 )
