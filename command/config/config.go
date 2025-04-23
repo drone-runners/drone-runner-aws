416b3faa9f7ff20dcbc93aad6146d312514fa375
Devansh Mathur <devanshmathur@harness.io>
2025-04-23 19:23:57 +0530

    Updating drone/plugin release version.

--- command/config/config.go
+++ command/config/config.go
@@ -347,7 +347,7 @@ type EnvConfig struct {
 		EnableAutoPool          bool   `envconfig:"DRONE_ENABLE_AUTO_POOL" default:"false"`
 		HarnessTestBinaryURI    string `envconfig:"DRONE_HARNESS_TEST_BINARY_URI"`
 		PluginBinaryURI         string `envconfig:"DRONE_PLUGIN_BINARY_URI" default:"https://github.com/drone/plugin/releases/download/v3.9.1-beta"`
-		PluginBinaryFallbackURI string `envconfig:"DRONE_PLUGIN_BINARY_FALLBACK_URI" default:"https://app.harness.io/storage/harness-download/harness-ti/harness-plugin/v3.9.1-beta"`
+		PluginBinaryFallbackURI string `envconfig:"DRONE_PLUGIN_BINARY_FALLBACK_URI" default:"https://app.harness.io/storage/harness-download/harness-ti/harness-plugin/v3.9.2-beta"`
 		PurgerTime              int64  `envconfig:"DRONE_PURGER_TIME_MINUTES" default:"30"`
 		AutoInjectionBinaryURI  string `envconfig:"DRONE_HARNESS_AUTO_INJECTION_BINARY_URI"`
 	}
