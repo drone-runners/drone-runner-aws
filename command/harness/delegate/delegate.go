63b9c9932b22b3dd91122279a9467f7e1bd5e53a
Soumyajit Das <soumyajit.das@harness.io>
2025-05-19 14:49:32 -0700

    fix: [CI-16878]: update autoinjection binary version

--- command/harness/delegate/delegate.go
+++ command/harness/delegate/delegate.go
@@ -91,7 +91,7 @@ func (c *delegateCommand) run(*kingpin.ParseContext) error {
 		env.Settings.HarnessTestBinaryURI = "https://app.harness.io/storage/harness-download/harness-ti/split_tests"
 	}
 	if env.Settings.AutoInjectionBinaryURI == "" {
-		env.Settings.AutoInjectionBinaryURI = "https://app.harness.io/storage/harness-download/harness-ti/auto-injection/1.0.3"
+		env.Settings.AutoInjectionBinaryURI = "https://app.harness.io/storage/harness-download/harness-ti/auto-injection/1.0.7"
 	}
 	c.env = env
 	// setup the global logrus logger.
