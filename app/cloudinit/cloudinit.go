43feceb1a75b250011278df4c0cd2e36ee89b99c
abhay084 <abhay.ganvir@harness.io>
2025-07-22 15:13:47 +0530

    fix: [CI-18056]: fix gofmt

--- app/cloudinit/cloudinit.go
+++ app/cloudinit/cloudinit.go
@@ -7,84 +7,84 @@
 package cloudinit
 
 import (
-        _ "embed"
-        "encoding/base64"
-        "fmt"
-        "path/filepath"
-        "strings"
-        "text/template"
+	_ "embed"
+	"encoding/base64"
+	"fmt"
+	"path/filepath"
+	"strings"
+	"text/template"
 
-        "github.com/drone-runners/drone-runner-aws/app/oshelp"
-        "github.com/drone-runners/drone-runner-aws/types"
+	"github.com/drone-runners/drone-runner-aws/app/oshelp"
+	"github.com/drone-runners/drone-runner-aws/types"
 )
 
 // Params defines parameters used to create userdata files.
 type Params struct {
-        LiteEnginePath          string
-        LiteEngineLogsPath      string
-        CACert                  string
-        TLSCert                 string
-        TLSKey                  string
-        Platform                types.Platform
-        HarnessTestBinaryURI    string
-        PluginBinaryURI         string
-        Tmate                   types.Tmate
-        IsHosted                bool
-        GitspaceAgentConfig     types.GitspaceAgentConfig
-        StorageConfig           types.StorageConfig
-        AutoInjectionBinaryURI  string
-        LiteEngineFallbackPath  string
-        PluginBinaryFallbackURI string
-        ShouldUseGoogleDNS      bool
-        DriverName              string
-        CertsDirectory          string
-        EnableC4D               bool
+	LiteEnginePath          string
+	LiteEngineLogsPath      string
+	CACert                  string
+	TLSCert                 string
+	TLSKey                  string
+	Platform                types.Platform
+	HarnessTestBinaryURI    string
+	PluginBinaryURI         string
+	Tmate                   types.Tmate
+	IsHosted                bool
+	GitspaceAgentConfig     types.GitspaceAgentConfig
+	StorageConfig           types.StorageConfig
+	AutoInjectionBinaryURI  string
+	LiteEngineFallbackPath  string
+	PluginBinaryFallbackURI string
+	ShouldUseGoogleDNS      bool
+	DriverName              string
+	CertsDirectory          string
+	EnableC4D               bool
 }
 
 var funcs = map[string]interface{}{
-        "base64": func(src string) string {
-                return base64.StdEncoding.EncodeToString([]byte(src))
-        },
-        "trim": strings.TrimSpace,
+	"base64": func(src string) string {
+		return base64.StdEncoding.EncodeToString([]byte(src))
+	},
+	"trim": strings.TrimSpace,
 }
 
 const certsDir = "/tmp/certs/"
 
 // Custom creates a custom userdata file.
 func Custom(templateText string, params *Params) (payload string, err error) {
-        t, err := template.New("custom-template").Funcs(funcs).Parse(templateText)
-        if err != nil {
-                err = fmt.Errorf("failed to parse template data: %w", err)
-                return "", err
-        }
+	t, err := template.New("custom-template").Funcs(funcs).Parse(templateText)
+	if err != nil {
+		err = fmt.Errorf("failed to parse template data: %w", err)
+		return "", err
+	}
 
-        sb := &strings.Builder{}
+	sb := &strings.Builder{}
 
-        caCertPath := filepath.Join(certsDir, "ca-cert.pem")
-        certPath := filepath.Join(certsDir, "server-cert.pem")
-        keyPath := filepath.Join(certsDir, "server-key.pem")
+	caCertPath := filepath.Join(certsDir, "ca-cert.pem")
+	certPath := filepath.Join(certsDir, "server-cert.pem")
+	keyPath := filepath.Join(certsDir, "server-key.pem")
 
-        err = t.Execute(sb, struct {
-                Params
-                CaCertPath string
-                CertPath   string
-                KeyPath    string
-                CertDir    string
-        }{
-                Params:     *params,
-                CaCertPath: caCertPath,
-                CertPath:   certPath,
-                KeyPath:    keyPath,
-                CertDir:    certsDir,
-        })
-        if err != nil {
-                err = fmt.Errorf("failed to execute template to get init script: %w", err)
-                return "", err
-        }
+	err = t.Execute(sb, struct {
+		Params
+		CaCertPath string
+		CertPath   string
+		KeyPath    string
+		CertDir    string
+	}{
+		Params:     *params,
+		CaCertPath: caCertPath,
+		CertPath:   certPath,
+		KeyPath:    keyPath,
+		CertDir:    certsDir,
+	})
+	if err != nil {
+		err = fmt.Errorf("failed to execute template to get init script: %w", err)
+		return "", err
+	}
 
-        payload = sb.String()
+	payload = sb.String()
 
-        return payload, nil
+	return payload, nil
 }
 
 //go:embed user_data/mac
@@ -128,163 +128,163 @@ var userDataWindows string
 var windowsTemplate = template.Must(template.New(oshelp.OSWindows).Funcs(funcs).Parse(userDataWindows))
 
 func Mac(params *Params) (payload string) {
-        sb := &strings.Builder{}
+	sb := &strings.Builder{}
 
-        caCertPath := filepath.Join(certsDir, "ca-cert.pem")
-        certPath := filepath.Join(certsDir, "server-cert.pem")
-        keyPath := filepath.Join(certsDir, "server-key.pem")
+	caCertPath := filepath.Join(certsDir, "ca-cert.pem")
+	certPath := filepath.Join(certsDir, "server-cert.pem")
+	keyPath := filepath.Join(certsDir, "server-key.pem")
 
-        var p = struct {
-                Params
-                CaCertPath string
-                CertPath   string
-                KeyPath    string
-        }{
-                Params:     *params,
-                CaCertPath: caCertPath,
-                CertPath:   certPath,
-                KeyPath:    keyPath,
-        }
+	var p = struct {
+		Params
+		CaCertPath string
+		CertPath   string
+		KeyPath    string
+	}{
+		Params:     *params,
+		CaCertPath: caCertPath,
+		CertPath:   certPath,
+		KeyPath:    keyPath,
+	}
 
-        if params.Platform.Arch == oshelp.ArchARM64 {
-                err := macArm64Template.Execute(sb, p)
-                if err != nil {
-                        err = fmt.Errorf("failed to execute mac arm64 template to get init script: %w", err)
-                        panic(err)
-                }
-        } else {
-                err := macTemplate.Execute(sb, p)
-                if err != nil {
-                        err = fmt.Errorf("failed to execute mac amd64 template to get init script: %w", err)
-                        panic(err)
-                }
-        }
-        return sb.String()
+	if params.Platform.Arch == oshelp.ArchARM64 {
+		err := macArm64Template.Execute(sb, p)
+		if err != nil {
+			err = fmt.Errorf("failed to execute mac arm64 template to get init script: %w", err)
+			panic(err)
+		}
+	} else {
+		err := macTemplate.Execute(sb, p)
+		if err != nil {
+			err = fmt.Errorf("failed to execute mac amd64 template to get init script: %w", err)
+			panic(err)
+		}
+	}
+	return sb.String()
 }
 
 // This generates a bash startup script for linux
 func LinuxBash(params *Params) (payload string) {
-        sb := &strings.Builder{}
+	sb := &strings.Builder{}
 
-        caCertPath := filepath.Join(certsDir, "ca-cert.pem")
-        certPath := filepath.Join(certsDir, "server-cert.pem")
-        keyPath := filepath.Join(certsDir, "server-key.pem")
+	caCertPath := filepath.Join(certsDir, "ca-cert.pem")
+	certPath := filepath.Join(certsDir, "server-cert.pem")
+	keyPath := filepath.Join(certsDir, "server-key.pem")
 
-        var p = struct {
-                Params
-                CaCertPath string
-                CertPath   string
-                CertDir    string
-                KeyPath    string
-        }{
-                Params:     *params,
-                CaCertPath: caCertPath,
-                CertDir:    certsDir,
-                CertPath:   certPath,
-                KeyPath:    keyPath,
-        }
+	var p = struct {
+		Params
+		CaCertPath string
+		CertPath   string
+		CertDir    string
+		KeyPath    string
+	}{
+		Params:     *params,
+		CaCertPath: caCertPath,
+		CertDir:    certsDir,
+		CertPath:   certPath,
+		KeyPath:    keyPath,
+	}
 
-        var err error
-        if (params.GitspaceAgentConfig.Secret != "" && params.GitspaceAgentConfig.AccessToken != "") ||
-                (params.GitspaceAgentConfig.VMInitScript != "") {
-                if params.GitspaceAgentConfig.VMInitScript != "" {
-                        decodedScript, decodeErr := base64.StdEncoding.DecodeString(params.GitspaceAgentConfig.VMInitScript)
-                        if decodeErr != nil {
-                                err = fmt.Errorf("failed to decode the gitspaces vm init script: %w", err)
-                                panic(err)
-                        }
-                        p.GitspaceAgentConfig.VMInitScript = string(decodedScript)
-                }
-                err = gitspacesLinuxTemplate.Execute(sb, p)
-        } else {
-                err = linuxBashTemplate.Execute(sb, p)
-        }
-        if err != nil {
-                err = fmt.Errorf("failed to execute linux bash template to get init script: %w", err)
-                panic(err)
-        }
-        return sb.String()
+	var err error
+	if (params.GitspaceAgentConfig.Secret != "" && params.GitspaceAgentConfig.AccessToken != "") ||
+		(params.GitspaceAgentConfig.VMInitScript != "") {
+		if params.GitspaceAgentConfig.VMInitScript != "" {
+			decodedScript, decodeErr := base64.StdEncoding.DecodeString(params.GitspaceAgentConfig.VMInitScript)
+			if decodeErr != nil {
+				err = fmt.Errorf("failed to decode the gitspaces vm init script: %w", err)
+				panic(err)
+			}
+			p.GitspaceAgentConfig.VMInitScript = string(decodedScript)
+		}
+		err = gitspacesLinuxTemplate.Execute(sb, p)
+	} else {
+		err = linuxBashTemplate.Execute(sb, p)
+	}
+	if err != nil {
+		err = fmt.Errorf("failed to execute linux bash template to get init script: %w", err)
+		panic(err)
+	}
+	return sb.String()
 }
 
 // Linux creates a userdata file for the Linux operating system.
 func Linux(params *Params) (payload string, err error) {
-        if params.CertsDirectory == "" {
-                params.CertsDirectory = certsDir
-        }
-        sb := &strings.Builder{}
-        caCertPath := filepath.Join(params.CertsDirectory, "ca-cert.pem")
-        certPath := filepath.Join(params.CertsDirectory, "server-cert.pem")
-        keyPath := filepath.Join(params.CertsDirectory, "server-key.pem")
-        templateData := struct {
-                Params
-                CaCertPath string
-                CertPath   string
-                KeyPath    string
-        }{
-                Params:     *params,
-                CaCertPath: caCertPath,
-                CertPath:   certPath,
-                KeyPath:    keyPath,
-        }
+	if params.CertsDirectory == "" {
+		params.CertsDirectory = certsDir
+	}
+	sb := &strings.Builder{}
+	caCertPath := filepath.Join(params.CertsDirectory, "ca-cert.pem")
+	certPath := filepath.Join(params.CertsDirectory, "server-cert.pem")
+	keyPath := filepath.Join(params.CertsDirectory, "server-key.pem")
+	templateData := struct {
+		Params
+		CaCertPath string
+		CertPath   string
+		KeyPath    string
+	}{
+		Params:     *params,
+		CaCertPath: caCertPath,
+		CertPath:   certPath,
+		KeyPath:    keyPath,
+	}
 
-        // Decode VMInitScript if provided
-        if params.GitspaceAgentConfig.VMInitScript != "" {
-                decoded, err := base64.StdEncoding.DecodeString(params.GitspaceAgentConfig.VMInitScript)
-                if err != nil {
-                        return "", fmt.Errorf("failed to decode the gitspaces vm init script: %w", err)
-                }
-                templateData.GitspaceAgentConfig.VMInitScript = string(decoded)
-        }
+	// Decode VMInitScript if provided
+	if params.GitspaceAgentConfig.VMInitScript != "" {
+		decoded, err := base64.StdEncoding.DecodeString(params.GitspaceAgentConfig.VMInitScript)
+		if err != nil {
+			return "", fmt.Errorf("failed to decode the gitspaces vm init script: %w", err)
+		}
+		templateData.GitspaceAgentConfig.VMInitScript = string(decoded)
+	}
 
-        // Select template
-        var tmpl *template.Template
-        switch params.Platform.OSName {
-        case oshelp.AmazonLinux:
-                if params.GitspaceAgentConfig.VMInitScript != "" {
-                        tmpl = gitspacesAmazonLinuxTemplate
-                } else {
-                        tmpl = amazonLinuxTemplate
-                }
-        default:
-                if params.GitspaceAgentConfig.VMInitScript != "" {
-                        if params.DriverName == string(types.Amazon) {
-                                tmpl = gitspacesAWSUbuntuTemplate
-                        } else {
-                                tmpl = gitspacesUbuntuTemplate
-                        }
-                } else {
-                        tmpl = ubuntuTemplate
-                }
-        }
-        // Execute selected template
-        if err := tmpl.Execute(sb, templateData); err != nil {
-                return "", fmt.Errorf("error while executing template: %w", err)
-        }
+	// Select template
+	var tmpl *template.Template
+	switch params.Platform.OSName {
+	case oshelp.AmazonLinux:
+		if params.GitspaceAgentConfig.VMInitScript != "" {
+			tmpl = gitspacesAmazonLinuxTemplate
+		} else {
+			tmpl = amazonLinuxTemplate
+		}
+	default:
+		if params.GitspaceAgentConfig.VMInitScript != "" {
+			if params.DriverName == string(types.Amazon) {
+				tmpl = gitspacesAWSUbuntuTemplate
+			} else {
+				tmpl = gitspacesUbuntuTemplate
+			}
+		} else {
+			tmpl = ubuntuTemplate
+		}
+	}
+	// Execute selected template
+	if err := tmpl.Execute(sb, templateData); err != nil {
+		return "", fmt.Errorf("error while executing template: %w", err)
+	}
 
-        return sb.String(), nil
+	return sb.String(), nil
 }
 
 // Windows creates a userdata file for the Windows operating system.
 func Windows(params *Params) (payload string) {
-        sb := &strings.Builder{}
+	sb := &strings.Builder{}
 
-        caCertPath := filepath.Join(certsDir, "ca-cert.pem")
-        certPath := filepath.Join(certsDir, "server-cert.pem")
-        keyPath := filepath.Join(certsDir, "server-key.pem")
+	caCertPath := filepath.Join(certsDir, "ca-cert.pem")
+	certPath := filepath.Join(certsDir, "server-cert.pem")
+	keyPath := filepath.Join(certsDir, "server-key.pem")
 
-        _ = windowsTemplate.Execute(sb, struct {
-                Params
-                CertDir    string
-                CaCertPath string
-                CertPath   string
-                KeyPath    string
-        }{
-                Params:     *params,
-                CertDir:    certsDir,
-                CaCertPath: caCertPath,
-                CertPath:   certPath,
-                KeyPath:    keyPath,
-        })
+	_ = windowsTemplate.Execute(sb, struct {
+		Params
+		CertDir    string
+		CaCertPath string
+		CertPath   string
+		KeyPath    string
+	}{
+		Params:     *params,
+		CertDir:    certsDir,
+		CaCertPath: caCertPath,
+		CertPath:   certPath,
+		KeyPath:    keyPath,
+	})
 
-        return sb.String()
+	return sb.String()
 }
