a5e37dff22d4e3136e58860ebed134c9181dd238
Ebtasam Faridy <ebtasam.faridy@harness.io>
2025-08-14 20:10:45 +0530

    feat: [CI-1888]: Lite-engine version update

--- vendor/github.com/drone/runner-go/handler/static/static.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Polyform License
-// that can be found in the LICENSE file.
-
-package static
-
-//go:generate togo http -package static -output static_gen.go
--- vendor/github.com/harness/ti-client/types/cache/buildcache/types.go
+++
@@ -1,8 +0,0 @@
-package buildcache
-
-type (
-	Metadata struct {
-		TotalTasks  int `json:"total_tasks"`
-		CachedTasks int `json:"cached_tasks"`
-	}
-)
