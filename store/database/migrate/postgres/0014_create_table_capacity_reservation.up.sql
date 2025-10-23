5643f435195fd735156694ae439b2df9f9941c59
Anurag Madnawat <anurag.madnawat@harness.io>
2025-10-23 14:41:20 -0700

    Update 0014_create_table_capacity_reservation.up.sql

--- store/database/migrate/postgres/0014_create_table_capacity_reservation.up.sql
+++ store/database/migrate/postgres/0014_create_table_capacity_reservation.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS capacity_reservation (
     ,pool_name         VARCHAR(250)
     ,instance_id       VARCHAR(250)
     ,reservation_id    VARCHAR(250)
-    ,created_at INTEGER NOT NULL,
+    ,created_at INTEGER NOT NULL
     );
 
 CREATE INDEX IF NOT EXISTS idx_capacity_reservation_pool_name ON capacity_reservation (pool_name);
