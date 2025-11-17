e6958d1a49986c940faf1ab17836d1945fb7b8df
Raghav <raghav.gupta@harness.io>
2025-11-18 05:13:58 +0530

    fix: [CI-19884]: Refactor healthcheck timeout based on driver (#708)
    * [CI-19884]: Refactor healthcheck timeout based on driver
    
    * [CI-19884]: Refactor healthcheck timeout based on driver

--- command/config/config.go
+++ command/config/config.go
@@ -344,10 +344,11 @@ type EnvConfig struct {
 	}
 
 	RunnerConfig struct {
-		HealthCheckTimeout        int64 `envconfig:"HEALTH_CHECK_TIMEOUT" default:"3"`
-		HealthCheckWindowsTimeout int64 `envconfig:"HEALTH_CHECK_WINDOWS_TIMEOUT" default:"5"`
-		SetupTimeout              int64 `envconfig:"SETUP_TIMEOUT" default:"30"`
-		HA                        bool  `envconfig:"DRONE_RUNNER_HA" default:"false"`
+		HealthCheckHotpoolTimeout   time.Duration `envconfig:"HEALTH_CHECK_HOTPOOL_TIMEOUT" default:"10s"`
+		HealthCheckColdstartTimeout time.Duration `envconfig:"HEALTH_CHECK_COLDSTART_TIMEOUT" default:"3m"`
+		HealthCheckWindowsTimeout   time.Duration `envconfig:"HEALTH_CHECK_WINDOWS_TIMEOUT" default:"5m"`
+		SetupTimeout                time.Duration `envconfig:"SETUP_TIMEOUT" default:"30s"`
+		HA                          bool          `envconfig:"DRONE_RUNNER_HA" default:"false"`
 	}
 
 	Nomad struct {
