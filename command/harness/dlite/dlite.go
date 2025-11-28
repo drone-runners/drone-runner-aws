c086c2862b2b8f27f5712b20db6eef3146608693
satyakota04 <satyakiran.kota@harness.io>
2025-11-28 12:30:27 +0530

    changed the auto-injection version

--- command/harness/dlite/dlite.go
+++ command/harness/dlite/dlite.go
@@ -98,7 +98,7 @@ func (c *dliteCommand) run(*kingpin.ParseContext) error {
 		env.Settings.HarnessTestBinaryURI = "https://app.harness.io/storage/harness-download/harness-ti/split_tests"
 	}
 	if env.Settings.AutoInjectionBinaryURI == "" {
-		env.Settings.AutoInjectionBinaryURI = "https://app.harness.io/storage/harness-download/harness-ti/auto-injection/1.0.11"
+		env.Settings.AutoInjectionBinaryURI = "https://app.harness.io/storage/harness-download/harness-ti/auto-injection/1.0.12"
 	}
 	c.env = env
 	// setup the global logrus logger.
