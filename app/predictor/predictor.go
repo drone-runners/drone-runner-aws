8b70b812d50f341c5a30fbaee6d37b105715b13f
Raghav <raghav.gupta@harness.io>
2025-12-02 02:20:32 +0530

    [CI-20004]: Added interface for predictor (#715)

---
+++ app/predictor/predictor.go
@@ -0,0 +1,28 @@
+package predictor
+
+import (
+	"context"
+)
+
+// PredictionInput contains the input data for making a prediction.
+type PredictionInput struct {
+	// PoolName is the name of the pool to predict for.
+	PoolName string
+}
+
+// PredictionResult contains the output of a prediction.
+type PredictionResult struct {
+	// RecommendedInstances is the recommended number of instances to have available.
+	RecommendedInstances int
+}
+
+// Predictor defines the interface for predicting the number of machines required.
+type Predictor interface {
+	// Predict predicts the number of machines required based on the given input.
+	// It returns a PredictionResult containing the recommended number of instances
+	// and related metadata.
+	Predict(ctx context.Context, input *PredictionInput) (*PredictionResult, error)
+
+	// Name returns the name of the predictor implementation.
+	Name() string
+}
