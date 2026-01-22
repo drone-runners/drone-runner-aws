32c9cba5c9e6703a5be6d810bbb86802dbdff272
Dhiraj Chhawchharia <dhiraj.chhawchharia@harness.io>
2026-01-22 15:46:35 +0530

    feat:[CI-19784]: Removed unnecessary comments

--- command/harness/dlite/init.go
+++ command/harness/dlite/init.go
@@ -16,7 +16,7 @@ import (
 
 const (
 	initTimeoutSec        = 30 * 60
-	initTimeoutSecForBYOI = 3 * 60 * 60 // 3 hours for BYOI image builds
+	initTimeoutSecForBYOI = 60 * 60
 )
 
 type VMInitTask struct {
